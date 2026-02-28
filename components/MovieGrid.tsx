e965ff79991ff559ff332cb93bc1d14d75bde3cf
carlesflor94 <cf.ferrandis@gmail.com>
2026-02-28 05:37:21 +0100

    css for MovieGrid

--- components/MovieGrid.tsx
+++ components/MovieGrid.tsx
@@ -1,5 +1,6 @@
 import { Movie } from "@/types/movie";
 import MovieCard from "./MovieCard";
+import styles from "./MovieGrid.module.css";
 
 type Props = {
   movies: Movie[];
@@ -8,7 +9,7 @@ type Props = {
 
 export default function MovieGrid({ movies, onRate }: Props) {
   return (
-    <div className="movies-container">
+    <div className={styles.container}>
       {movies.slice(0, 6).map((movie) => (
         <MovieCard key={movie.id} movie={movie} onRate={onRate} />
       ))}
