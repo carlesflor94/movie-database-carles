8ef8ec357dc2f09de6186827cf1a179b57e36d95
carlesflor94 <cf.ferrandis@gmail.com>
2026-03-04 16:44:30 +0100

    added genrecontext.tsx

---
+++ context/GenreContext.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import { useEffect, createContext, useContext, useState } from "react";
+
+type Genre {
+    id: number;
+    name: string;
+}
+
+type GenreContextType {
+    genres: Genre[];
+    getGenreName: (id: number) => string;
+}
+
+const GenreContext = createContext<GenreContextType | undefined>(undefined);
+
+export function getGenre({ children }: { children: React.ReactNode }) {
+    const [genres, setGenres] = useState<Genre[]>([]);
+
+    useEffect(() => {
+        async function fetchGenres() {
+            try {
+                const res = await fetch("/api/movies/genres");
+                const data = await res.json();
+                setGenres(data.genres);
+            } catch (err) {
+                console.error("Failed to fetch genres", err);
+            }
+        }
+
+        fetchGenres();
+    }, []);
+
+    const getGenreName = (id: number) => {
+        const genre = genres.find((g) => g.id === id);
+        return genre ? genre.name : "";
+    }
+
+    return (
+        <GenreContext.Provider value={{ genres, getGenreName }}>
+            {children}
+        </GenreContext.Provider>
+    )
+}
+
+export function useGenres() {
+    const context = useContext(GenreContext);
+
+    if (!context) {
+        throw new Error("useGenres must be inside getGenre");
+    }
+
+    return context;
+}
